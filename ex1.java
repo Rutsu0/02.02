bf1b6ed4cd924241059d51b50bca574d9a62b36e
Nikita <nekit2007003lol@gmail.com>
2026-09-03 13:20:55 +0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,33 @@
+//№1
+//public class Main {
+//    public static void main(String[] args) {
+//        double rubles = 30002.7;
+//        double rateUSD = 87;
+//        double dollars = rubles/rateUSD;
+//        System.out.printf("У вас на счету " + rubles + " рублей. В долларах это " + dollars + " долларов. Так держать!");
+//    }
+//}
+//№2
+//public class Main {
+//public static void main(String[] args) {
+//    String weather = "Дождь";
+//    if (weather.equals("Дождь")){
+//        System.out.println("Беру с собой зонт");
+//    } else {
+//        System.out.println("Иду налегке");
+//    }
+//    System.out.println("Люблю гулять!");
+//    }
+//}
+//№3
+//public class Main {
+//    public static void main(String[] args) {
+//        int eggs = 5;
+//        if (eggs > 3){
+//            System.out.println("Рекомендую приготовить омлет");
+//        } else {
+//            System.out.println("Рекомендую позавтракать бутербродами");
+//        }
+//        System.out.println("Приятного аппетита!");
+//    }
+//}
